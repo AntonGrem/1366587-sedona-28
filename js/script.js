44bb38b54838de091f0a7a71bca3c82eb24ad831
AntonGrem <AntonGrem159@rambler.ru>
2020-06-15 14:31:54 +0300

    23123123123

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 var buttonSearchForm = document.querySelector(".form__button-search");
-var formSearching =document.querySelector(".form__searching-invisible");
+var formSearching = document.querySelector(".form__searching-invisible");
+var formButtonFind = document.querySelector(".form__button-find");
+
 buttonSearchForm.addEventListener("click", function(event){
     event.preventDefault()
     formSearching.classList.toggle("form__searching-visible");
 });
-
+formButtonFind.addEventListener("click", function(event){
+  event.preventDefault()  
+});
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       if (formSearching.classList.contains("form__searching-visible")) {
