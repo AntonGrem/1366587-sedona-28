8cc9af5e5504106bc81d486d990950d60ad7b2e7
AntonGrem <AntonGrem159@rambler.ru>
2020-06-17 16:48:43 +0300

    qwerty

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 var buttonSearchForm = document.querySelector(".form__link-search");
-var formSearching = document.querySelector(".form__searching-invisible");
+var formSearching = document.querySelector(".form__searching-visible");
 var formButtonFind = document.querySelector(".form__button-find");
 
 buttonSearchForm.addEventListener("click", function(event){
     event.preventDefault()
-    formSearching.classList.toggle("form__searching-visible");
+    if (formSearching.classList.contains("form__searching-visible")){
+      formSearching.classList.remove("form__searching-visible");
+      formSearching.classList.add("form__searching-invisible");
+    }
+    else{
+      formSearching.classList.remove("form__searching-invisible");
+      formSearching.classList.add("form__searching-visible");
+    }
+    
 });
 formButtonFind.addEventListener("click", function(event){
   event.preventDefault()  
@@ -14,6 +22,7 @@ window.addEventListener("keydown", function (evt) {
       if (formSearching.classList.contains("form__searching-visible")) {
         evt.preventDefault();
         formSearching.classList.remove("form__searching-visible");
+        formSearching.classList.add("form__searching-invisible");
       }
     }
   });
